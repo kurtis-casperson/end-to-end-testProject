1432f9dfa033684ddc852cad1e97e8d7de1354a4
Kurtis <kurtis@edge.app>
2022-07-12 20:48:57 -0700

    Add Cypress testing set up

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const cypress = require('cypress')
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5501',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
